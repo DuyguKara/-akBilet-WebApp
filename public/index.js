6d754e76bac11845b1de694ff1477e0ca08d1fc5
Duygu <duygukara_2799@hotmail.com>
2024-11-27 00:34:59 +0300

    Adding 'Add Event Modal html and css files'

--- public/index.js
+++ public/index.js
@@ -1,11 +1,1 @@
-/* document.getElementById("sign-btn").addEventListener("click", function(){
-    fetch('/sign-in', {
-        method: 'GET',
-    }).then(response => {
-        if (response.ok) {
-            window.location.href = '/sign-in';
-        } else {
-            console.log('Yönlendirme hatası');
-        }
-    }).catch(error => console.error('Bir hata oluştu:', error));
-}); */
+document.querySelector('.add-event').addEventListener
